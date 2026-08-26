283c5f0ef94282acbe251b487637efadafc9ca14
Neil Taïson Rigaud <112826785+ntsrigaud@users.noreply.github.com>
2026-08-26 22:06:00 +0800

    Add: three-integer-sum - submission-3

---
+++ Data Structures & Algorithms/three-integer-sum/submission-3.cpp
@@ -0,0 +1,38 @@
+class Solution {
+   public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int left = 0;
+        int right = 0;
+        int sum = 0;
+        std::vector<std::vector<int>> triplets;
+
+        std::ranges::sort(nums);
+        for (size_t i = 0; i < nums.size() - 2; ++i) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;  // Skip duplicates
+            }
+
+            // Two-Sum
+            left = i + 1;
+            right = nums.size() - 1;
+            sum = 0;
+            while (left < right) {
+                sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    triplets.push_back({nums[i], nums[left], nums[right]});
+                    // Update our pointers to continue for all the possible combinations
+                    ++left;
+                    while (left < right && nums[left] == nums[left - 1]) {
+                        ++left;
+                    }
+                } else if (sum < 0) {
+                    ++left;  // Increase sum
+                } else {
+                    --right;  // Decrease sum
+                }
+            }
+        }
+
+        return triplets;
+    }
+};
