1d7ec45a4cf507d05955cbeaeb30f01313533b59
Neil Taïson Rigaud <112826785+ntsrigaud@users.noreply.github.com>
2026-08-30 11:58:50 +0800

    Add: max-water-container - submission-3

---
+++ Data Structures & Algorithms/max-water-container/submission-3.cpp
@@ -0,0 +1,27 @@
+class Solution {
+   public:
+    int maxArea(vector<int>& heights) {
+        size_t left = 0;
+        size_t right = heights.size() - 1;
+        int max_area = 0;
+        int new_area = 0;
+        int min_h = 0;
+
+        while (left < right) {
+            min_h = std::min(heights[left], heights[right]);
+            new_area = min_h * (right - left);
+            max_area = std::max(max_area, new_area);
+
+            // Fast skipping for shorter lines to avoid redundant area computations
+            while (left < right && heights[left] <= min_h) {
+                ++left;
+            }
+
+            while (left < right && heights[right] <= min_h) {
+                --right;
+            }
+        }
+
+        return max_area;
+    }
+};
