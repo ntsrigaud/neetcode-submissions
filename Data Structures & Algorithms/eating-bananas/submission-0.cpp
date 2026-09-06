a6d2ae49884249bcaab62199eebbceefe41ec17c
Neil Taïson Rigaud <112826785+ntsrigaud@users.noreply.github.com>
2026-09-06 11:49:34 +0800

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.cpp
@@ -0,0 +1,33 @@
+class Solution {
+   public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        if (piles.empty()) return 0;
+
+        int low = 1;
+        int high = std::ranges::max(piles);  // Get max eating speed
+
+        auto getEatingTime = [&](int eating_speed) -> long long {
+            long long total_time = 0;
+
+            for (int p : piles) {
+                total_time += (p / eating_speed) + (p % eating_speed != 0);
+            }
+
+            return total_time;
+        };
+
+        while (low < high) {
+            const auto mid = low + (high - low) / 2;
+
+            // Half-open interval -> [low, high)
+            if (getEatingTime(mid) <= h) {
+                // Speed is too fast
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return low;  // Points to the min valid speed at the end of search
+    }
+};
