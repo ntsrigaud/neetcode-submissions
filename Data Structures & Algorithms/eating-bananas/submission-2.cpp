d1253d4e8ca7f2c1b1a523c5efa13e2e062b4141
Neil Taïson Rigaud <112826785+ntsrigaud@users.noreply.github.com>
2026-09-06 12:31:40 +0800

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.cpp
@@ -0,0 +1,28 @@
+class Solution {
+   public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        if (piles.empty() || h < static_cast<int>(piles.size())) return 0;
+
+        int low = 1;
+        int high = std::ranges::max(piles);
+
+        while (low < high) {
+            const auto mid = low + (high - low) / 2;
+
+            // Get the total eating time
+            int eating_hours = 0;
+            for (int p : piles) {
+                eating_hours += std::ceil(static_cast<double>(p) / mid);
+            }
+
+            if (eating_hours <= h) {
+                // Eating rate might be too fast -> Shift to the left
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+};
