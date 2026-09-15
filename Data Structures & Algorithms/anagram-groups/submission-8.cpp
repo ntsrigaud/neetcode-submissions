2d44b3ad1a0eab2eceb751e45ce7dee8af16e34e
Neil Taïson Rigaud <112826785+ntsrigaud@users.noreply.github.com>
2026-09-15 22:42:38 +0800

    Add: anagram-groups - submission-8

---
+++ Data Structures & Algorithms/anagram-groups/submission-8.cpp
@@ -0,0 +1,21 @@
+class Solution {
+   public:
+    std::vector<std::vector<std::string>> groupAnagrams(std::vector<std::string>& strs) {
+        // Use sorted letters of each words as key
+        std::unordered_map<std::string, std::vector<std::string>> str_map;
+        for (auto& str : strs) {
+            auto key = str;
+            std::ranges::sort(key);
+
+            str_map[key].push_back(str);
+        }
+
+        // Collect the collected sublists of anagrams
+        std::vector<std::vector<std::string>> anagram_lists;
+        for (auto& [key, sublist] : str_map) {
+            anagram_lists.emplace_back(std::move(sublist));
+        }
+
+        return anagram_lists;
+    }
+};
