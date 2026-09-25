50e8c49792960a4d1acd11fe49d4a351165c0e8a
Neil Taïson Rigaud <112826785+ntsrigaud@users.noreply.github.com>
2026-09-25 14:03:39 +0800

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    static constexpr int ENGLISH_LETTERS = 26;
+
+   public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size()) return false;
+
+        // Fixed array for counting char frequencies
+        std::array<int, ENGLISH_LETTERS> count{}; // Zero-init
+        int len = static_cast<int>(s.size());
+
+        // Record freqencies across both strings
+        for (int i = 0; i < len; ++i) {
+            ++count[s[i] - 'a'];
+            --count[t[i] - 'a'];
+        }
+
+        for (int i = 0; i < ENGLISH_LETTERS; ++i) {
+            if (count[i] != 0) return false;
+        }
+
+        return true;
+    }
+};
